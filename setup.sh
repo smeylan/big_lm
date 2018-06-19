c1aff4b1cfb36b76e51b2f97e2271cbaaa63086e
Stephan Meylan <meylan.stephan@gmail.com>
2018-06-19 07:06:25 -0700

    added missing ckpt files to setup.sh

--- setup.sh
+++ setup.sh
@@ -8,6 +8,8 @@ mkdir data
 mkdir output
 touch WORKSPACE
 wget http://download.tensorflow.org/models/LM_LSTM_CNN/graph-2016-09-10.pbtxt
+wget http://download.tensorflow.org/models/LM_LSTM_CNN/all_shards-2016-09-10/ckpt-base
+wget http://download.tensorflow.org/models/LM_LSTM_CNN/all_shards-2016-09-10/ckpt-char-embedding
 wget http://download.tensorflow.org/models/LM_LSTM_CNN/all_shards-2016-09-10/ckpt-softmax2
 wget http://download.tensorflow.org/models/LM_LSTM_CNN/all_shards-2016-09-10/ckpt-softmax3
 wget http://download.tensorflow.org/models/LM_LSTM_CNN/all_shards-2016-09-10/ckpt-softmax4
